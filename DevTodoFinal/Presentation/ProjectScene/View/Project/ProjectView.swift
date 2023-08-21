0cc5e4d7f17b31d2cfa320268a56a7cf9226df99
Anniekkang <knr9144@gmail.com>
2023-08-21 19:43:54 +0900

    SceneDelegate에 AppCoordinator를 적용하여 화면설계

--- DevTodoFinal/Presentation/ProjectScene/View/Project/ProjectView.swift
+++ DevTodoFinal/Presentation/ProjectScene/View/Project/ProjectView.swift
@@ -10,6 +10,36 @@ import SnapKit
 
 class ProjectView: BaseView {
     
+    let collectionView : UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        return view
+    }()
    
 
+    override func configure() {
+        self.addSubview(collectionView)
+        collectionView.register(ProjectCollectionViewCell.self, forCellWithReuseIdentifier: ProjectCollectionViewCell.id)
+    }
+    
+    override func setConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(25)
+        }
+    }
+}
+
+extension ProjectView {
+    static func collectionViewLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        let deviceWidth: CGFloat = UIScreen.main.bounds.width
+        let itemWidth: CGFloat = (deviceWidth / 2) - 35
+        layout.sectionInset = UIEdgeInsets(top: 30, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        layout.scrollDirection = .vertical
+        return layout
+    }
+    
 }
